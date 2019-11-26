efce24d1fbbbe9a51db5896b31e1112b74989c84
MicahW <micah.wallberg@gmail.com>
2019-11-26 14:50:02 -0500

    Added skeleton server

---
+++ Server.py
@@ -0,0 +1,32 @@
+# The Tron server
+class Server:
+
+    # Set up data structures and open UDP server
+    def __init__(self):
+        pass
+
+    # Start listening for conecting messages, create new clients send connected message
+    # Ensures addtional connecting message from the same client does not 
+    #   produce multiple clients in the game state
+    def listen_for_connections(self);
+        pass
+
+    # Send the game starting message to all players and ensure a ack was received
+    def send_game_starting(self):
+        pass
+
+    # Listen for player direction messages
+    def get_direction_inputs(self):
+        pass
+
+    # Update the game state
+    def update_game_state(self):
+        pass
+
+    # Send out game state, dead, and winner messages
+    def send_game_state(self):
+        pass
+
+    # Start updating the game state and waiting for player direction messages
+    def start_game(self):
+        pass
