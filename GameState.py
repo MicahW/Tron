8b6ce7b2ea9013960cdb4b621020fe35a03c68d3
MicahW <micah.wallberg@gmail.com>
2019-11-30 10:38:03 -0500

    Implemented GameState's tick method

--- GameState.py
+++ GameState.py
@@ -5,6 +5,13 @@ SIZE_Y = 200
 # Holds board values and player head values
 class GameState:
 
+    direction_map = {
+        0: [-1, 0],
+        1: [0, -1],
+        2: [1, 0],
+        3: [0, 1]
+    }
+
     # Create empty board and player states
     def __init__(self):
         self.board = [[0] * SIZE_Y] * SIZE_X
@@ -18,8 +25,26 @@ class GameState:
             players[player_number][2] = direction
 
     # Called by server to update the game state
+    # Returns a list of dead players
     def tick(self):
-        pass
+        # Move each head then add it to the baord
+        for player_number, (head_x, head_y, direction, alive) in self.players.items():
+            if alive:
+                delta_x, delta_y = direction_map[direction]
+                head_x = head_x + delta_x
+                head_y = head_x + delta_y
+                self.players[player_number] = [head_x, head_y, direction, alive]
+                self.board[head_x][head_y] = player_number
+
+        # Check if any heads colided into a wall
+        dead_list = []
+        for player_number, (head_x, head_y, direction, alive) in self.players.items():
+            if alive:
+                if (board[head_x][head_y] != 0):
+                    dead_list.append(player_number)
+                    self.players[player_number] = [head_x, head_y, direction, False]
+
+        return dead_list
 
     # Called by server to add a new player, returns player number
     def create_new_player(self):
